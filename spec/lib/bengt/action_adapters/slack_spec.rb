ca802b680a15a558f484f71520e7d8c04e04d6c5
Magnus Skog <magnus.m.skog@gmail.com>
2015-05-07 22:39:02 +0200

    Slack adapter accepts a custom method for the post

--- spec/lib/bengt/action_adapters/slack_spec.rb
+++ spec/lib/bengt/action_adapters/slack_spec.rb
@@ -4,17 +4,20 @@ require 'bengt/action_adapters/slack'
 describe Bengt::ActionAdapters::Slack do
   subject{described_class.new}
 
+  Given(:data){JSON.parse(File.read('spec/fixtures/imagepost.json'))}
+  Given(:post){Bengt::Models::Post.new(data)}
+
   describe "#perform" do
     context "with configuration from hash in the initializer" do
       Given(:url){'http://www.example.com/'}
       Given(:username){'foobar'}
       Given(:channel){'#default'}
-      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"channel\":\"#default\",\"username\":\"foobar\",\"text\":\"http://www.cnn.com\"}"})}
-      Given(:payload){{url: :'http://www.cnn.com'}}
+      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"channel\":\"#default\",\"username\":\"foobar\",\"text\":\"#{payload}\"}"})}
+      Given(:payload){'http://imgur.com/krDq9fs.jpg'}
 
       subject{described_class.new(url: url, username: username, channel: channel)}
 
-      When{subject.perform(payload)}
+      When{subject.perform(post)}
 
       Then{expect(stub).to have_been_requested}
     end
@@ -23,20 +26,20 @@ describe Bengt::ActionAdapters::Slack do
       Given(:url){'http://www.example.com/'}
       Given(:username){'foobar'}
       Given(:channel){'#default'}
-      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"channel\":\"#default\",\"username\":\"foobar\",\"text\":\"http://www.cnn.com\"}"})}
-      Given(:payload){{url: :'http://www.cnn.com'}}
+      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"channel\":\"#default\",\"username\":\"foobar\",\"text\":\"#{payload}\"}"})}
+      Given(:payload){'http://imgur.com/krDq9fs.jpg'}
 
       Given{subject.configure(url: url, username: username, channel: channel)}
 
-      When{subject.perform(payload)}
+      When{subject.perform(post)}
 
       Then{expect(stub).to have_been_requested}
     end
 
     context "with block configuration" do
       Given(:url){'http://www.example.com/'}
-      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"text\":\"http://www.cnn.com\"}"})}
-      Given(:payload){{url: :'http://www.cnn.com'}}
+      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"text\":\"#{payload}\"}"})}
+      Given(:payload){'http://imgur.com/krDq9fs.jpg'}
 
       Given do
         subject.configure do |config|
@@ -44,9 +47,24 @@ describe Bengt::ActionAdapters::Slack do
         end
       end
 
-      When{subject.perform(payload)}
+      When{subject.perform(post)}
 
       Then{expect(stub).to have_been_requested}
+    end
+
+    context "with a custom method" do
+      Given(:url){'http://www.example.com/'}
+      Given(:username){'foobar'}
+      Given(:channel){'#default'}
+      Given!(:stub){stub_request(:post, url).with(:body => {"payload"=>"{\"channel\":\"#default\",\"username\":\"foobar\",\"text\":\"#{payload}\"}"})}
+      Given(:payload){"/r/pics/comments/31mbii/sunrise_in_cincinnati/"}
+
+      subject{described_class.new(url: url, username: username, channel: channel, post_method: :permalink)}
+
+      When{subject.perform(post)}
+
+      Then{expect(stub).to have_been_requested}
+
     end
   end
 end
