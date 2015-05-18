798132a7d421c95274cc24bc0976bf35c0f70ce9
Magnus Skog <magnus.m.skog@gmail.com>
2015-05-18 21:49:47 +0200

    Soooo lazy

--- lib/bengt.rb
+++ lib/bengt.rb
@@ -1,10 +1,14 @@
 require "bengt/version"
+require 'require_all'
 require 'faraday'
 require 'virtus'
 require 'json'
 
 require 'bengt/configuration'
-require 'bengt/decorators/cheapskate'
+require 'bengt/models/post'
+require 'bengt/filter'
+
+autoload_all File.dirname(__FILE__) + "/bengt"
 
 module Bengt
 end
